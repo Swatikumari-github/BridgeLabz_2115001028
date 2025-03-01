8a107466117d0bfa0ec8567cf5736de91fe65bc1
Swatikumari-github <106343290+Swatikumari-github@users.noreply.github.com>
2025-03-01 16:09:26 +0530

    [swati]Add. adding project 1 of user registration

---
+++ ModelLayer/DTO/RegisterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ModelLayer.DTO
+{
+    public class RegisterDTO
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
